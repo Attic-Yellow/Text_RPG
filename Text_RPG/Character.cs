1c546098110d0de6fa70f38a435311c1c9ceb40a
loftdoggy <chtori1202@gmail.com>
2023-11-07 04:05:53 +0900

    캐릭터 직업 추가 : 전사, 마법사, 궁수 추가

--- Text_RPG/Character.cs
+++ Text_RPG/Character.cs
@@ -15,4 +15,35 @@ namespace Text_RPG
         // 추가적인 스탯, 능력치, 장비 등의 속성
     }
 
+    public class Warrior : Character
+    {
+        public Warrior()
+        {
+            this.Health = 100;
+            this.Attack = 15;
+            // ... 다른 속성 초기화
+        }
+    }
+
+    public class Mage : Character
+    {
+        public Mage()
+        {
+            this.Health = 80;
+            this.Attack = 10;
+            this.Magic = 20;
+            // ... 다른 속성 초기화
+        }
+    }
+
+    public class Archer : Character
+    {
+        public Archer()
+        {
+            this.Health = 90;
+            this.Attack = 12;
+            // ... 다른 속성 초기화
+        }
+    }
+
 }
