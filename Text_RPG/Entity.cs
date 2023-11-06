a0040858a5efb9a4051ceebc5f9dd4b83ddebb04
AtticYellow <ziouLTE@gmail.com>
2023-11-07 00:13:24 +0900

    엔티티 내용 추가 : 레벨 추가

--- Text_RPG/Entity.cs
+++ Text_RPG/Entity.cs
@@ -11,6 +11,8 @@ namespace Text_RPG
         public string Name { get; set; }
         public int Health { get; set; }
         public int MaxHealth { get; set; }
+        public int Level { get; set; } 
+
     }
 
 }
