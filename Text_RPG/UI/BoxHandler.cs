9fb392311b6661bbaec4fcb9a8494783c944e55c
loftdoggy <chtori1202@gmail.com>
2023-11-08 01:14:36 +0900

    Box 클래스 추가 : StartUI, MainPageUI 공통된 클래스 통합

---
+++ Text_RPG/UI/BoxHandler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_RPG.UI
+{
+    public class Box
+    {
+        public List<string> Menus { get; set; }
+        public int SelectedIndex { get; private set; } = 0;
+        public int Width { get; set; } = 40; // 기본 너비 설정
+        public int Padding { get; set; } = 2; // 내부 패딩 설정
+
+        public Box(List<string> menus)
+        {
+            Menus = menus;
+        }
+
+        public void NextItem()
+        {
+            SelectedIndex = (SelectedIndex + 1) % Menus.Count;
+        }
+
+        public void PreviousItem()
+        {
+            SelectedIndex = (SelectedIndex - 1 + Menus.Count) % Menus.Count;
+        }
+    }
+
+    public class BoxHandler
+    {
+        public Box Box { get; private set; }
+
+        public BoxHandler(List<string> menuNumbers, int width = 40)
+        {
+            Box = new Box(menuNumbers) { Width = width };
+        }
+
+        public void Display()
+        {
+            int topOffset = (Console.WindowHeight - Box.Menus.Count) / 2;
+
+            for (int i = 0; i < Box.Menus.Count; i++)
+            {
+                Console.SetCursorPosition((Console.WindowWidth - Box.Width) / 2 + 2, topOffset + i + 1);
+
+                if (i == Box.SelectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    UIRender.DrawCenteredStringInBox($"{Box.Menus[i]} ◀", Box.Width);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    UIRender.DrawCenteredStringInBox($"  {Box.Menus[i]}", Box.Width);
+                }
+            }
+        }
+
+        public void Navigate(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    Box.PreviousItem();
+                    break;
+                case ConsoleKey.DownArrow:
+                    Box.NextItem();
+                    break;
+            }
+        }
+    }
+
+}
