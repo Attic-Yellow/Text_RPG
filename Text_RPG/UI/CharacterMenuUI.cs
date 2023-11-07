070e38e1dff1861c2d9cb42a2764f3a2f667d8f3
loftdoggy <chtori1202@gmail.com>
2023-11-08 03:00:00 +0900

    CharacterMenuUI 추가 기능 구현 : 캐릭터 선택 후 직업 출력 및 선택 구현

--- Text_RPG/UI/CharacterMenuUI.cs
+++ Text_RPG/UI/CharacterMenuUI.cs
@@ -11,7 +11,7 @@ namespace Text_RPG.UI
     {
         public List<string> Menus { get; set; }
         public int SelectedIndex { get; private set; } = 0;
-        public int Width { get; set; } = 100; // 기본 너비 설정
+        public int Width { get; set; } = 110; // 기본 너비 설정
         public int Padding { get; set; } = 4; // 내부 패딩 설정
 
         public GameBox(List<string> menus)
@@ -34,15 +34,15 @@ namespace Text_RPG.UI
     {
         public Box Box { get; private set; }
 
-        public GameBoxHandler(List<string> menuNumbers, int width = 100)
+        public GameBoxHandler(List<string> menuNumbers, int width = 110)
         {
             Box = new Box(menuNumbers) { Width = width };
         }
 
         public void Display()
         {
-            int topOffset = (Console.WindowHeight - Box.Menus.Count) / 2 - 7;
-            int leftOffset = (Console.WindowWidth - Box.Width) / 2 + 6; 
+            int topOffset = (Console.WindowHeight - Box.Menus.Count) / 2 - 8;
+            int leftOffset = (Console.WindowWidth - Box.Width) / 2 + 5; 
             int spaceBetween = (Box.Width - Box.Menus.Sum(s => s.Length)) / (Box.Menus.Count);
 
             Console.SetCursorPosition(leftOffset, topOffset);
@@ -52,7 +52,7 @@ namespace Text_RPG.UI
                 if (i == Box.SelectedIndex)
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write(Box.Menus[i]);
+                    Console.Write(Box.Menus[i]+" ◀");
                     Console.ResetColor();
                 }
                 else
@@ -68,6 +68,42 @@ namespace Text_RPG.UI
             Console.WriteLine(); 
         }
 
+        public void SecondDisplay(InputHandler inputHandler, CharacterSelectionUI characterUI, int selectedIndex)
+        {
+            int topOffset = (Console.WindowHeight - Box.Menus.Count) / 2 - 5;
+            int leftOffset = (Console.WindowWidth - Box.Width) / 2 + 8;
+
+            Console.SetCursorPosition(leftOffset, topOffset);
+
+            var selectedCharacters = characterUI.DisplayCharacterSelection(inputHandler);
+            int nameWidth = selectedCharacters.Max(sc => sc.Key.Length) + 4; 
+
+            for (int i = 0; i < selectedCharacters.Count; i++)
+            {
+                var character = selectedCharacters.ElementAt(i);
+                var key = character.Key.PadRight(nameWidth);
+                var value = character.Value;
+
+                if (i == selectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write($"{key}◀");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write($"{key}");
+                }
+
+                if (i < selectedCharacters.Count - 1)
+                {
+                    Console.Write(new string(' ', nameWidth));
+                }
+            }
+            Console.WriteLine();
+        }
+
+
         public void Navigate(ConsoleKey key)
         {
             switch (key)
@@ -81,6 +117,7 @@ namespace Text_RPG.UI
             }
         }
     }
+
     class CharacterMenuUI
     {
         private static readonly List<string> menuNumber = new List<string>
@@ -113,12 +150,50 @@ namespace Text_RPG.UI
             }
         }
 
+        public static void DisPlayCharacter(InputHandler inputHandler, CharacterSelectionUI characterUI, int selectedIndex)
+        {
+            bool conti = true;
+            int selectedCharactersCount = characterUI.DisplayCharacterSelection(inputHandler).Count;
+
+            while (conti)
+            {
+                Console.Clear();
+                UIRender.DrawCharacterMenuBox(menuBoxHandler.Box.Width, menuBoxHandler.Box.Menus.Count);
+                menuBoxHandler.Display();
+                menuBoxHandler.SecondDisplay(inputHandler, characterUI, selectedIndex);
+
+                var key = inputHandler.GetUserInput();
+
+                if (key == ConsoleKey.LeftArrow && selectedIndex > 0)
+                {
+                    selectedIndex--;  
+                }
+                else if (key == ConsoleKey.RightArrow && selectedIndex < selectedCharactersCount - 1)
+                {
+                    selectedIndex++;  
+                }
+
+                if (key == ConsoleKey.Enter)
+                {
+                   
+                    CharacterStatusPerformAction(inputHandler, characterUI, selectedIndex);
+                    conti = false; 
+                }
+                else if (key == ConsoleKey.V) 
+                {
+                    DisplayCaharcterMenu(inputHandler, characterUI);
+                    conti = false;
+                }
+            }
+        }
+
         private static void CharacterPerformAction(InputHandler inputHandler, CharacterSelectionUI characterUI, int selectedIndex)
         {
             switch (selectedIndex)
             {
-                case 0: 
-                    Console.Clear();
+                case 0:
+                    DisPlayCharacter(inputHandler, characterUI, selectedIndex);
+
                     break;
                 case 1: 
                     break;
@@ -127,6 +202,21 @@ namespace Text_RPG.UI
                 case 3:
                     break;
                 case 4:
+                    MainPageUI.DisplayMainPageMenu(inputHandler, characterUI);
+                    break;
+            }
+        }
+
+        private static void CharacterStatusPerformAction(InputHandler inputHandler, CharacterSelectionUI characterUI, int selectedIndex)
+        {
+            switch (selectedIndex)
+            {
+                case 0:
+                    DisPlayCharacter(inputHandler, characterUI, selectedIndex);
+                    break;
+                case 1:
+                    break;
+                case 2:
                     MainPageUI.DisplayMainPageMenu(inputHandler, characterUI);
                     break;
             }
