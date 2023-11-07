1714bafb48d7dd8511005d635258209360b910cb
loftdoggy <chtori1202@gmail.com>
2023-11-07 21:19:13 +0900

    엔티티 관련 파일 폴더화 :

---
+++ Text_RPG/Entity/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_RPG.Entity
+{
+    public class Entity
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int Level { get; set; }
+        public int Magic { get; set; }
+    }
+
+}
---
+++ Text_RPG/Entity/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_RPG.Entity
+{
+    public class Item
+    {
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public string Property { get; set; }
+        // 아이템의 효과, 타입, 사용 조건 등의 속성
+    }
+}
