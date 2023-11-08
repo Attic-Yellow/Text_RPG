82dcacef61a19c532203f8d24face5fc046a0319
loftdoggy <chtori1202@gmail.com>
2023-11-09 00:41:22 +0900

    InventoryMenuItem 클래스 생성 및 기능 구현 : 아이템 출력 및 선택 기능 구현(아이템 데이터 없음 임의로 기능만 구현한 상태)

---
+++ Text_RPG/UI/GameMenuUI/InventoryUI/InventoryMenuItemUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Text_RPG.UI.GameMenuBoxHandler;
+using Text_RPG.Util;
+
+namespace Text_RPG.UI.GameMenuUI.InventoryUI
+{
+    class InventoryMenuItemUI : InventoryMenuUI
+    {
+        private static readonly List<string> InventoryItemIndex = new List<string> { "아이템 1", "아이템 2" };
+
+        protected static readonly GameBoxHandler InventoryMenuItemBoxHandler = new GameBoxHandler(InventoryItemIndex);
+
+        public static void InventoryMenuItemDisplay(InputHandler inputHandler, CharacterSelectionUI characterUI)
+        {
+            bool conti = true;
+            while (conti)
+            {
+                Console.Clear();
+                UIRender.DrawCharacterMenuBox(menuBoxHandler.Box.Width, menuBoxHandler.Box.GameMenus.Count);
+                UIRender.DrawInventoryMenuWorkBox(menuBoxHandler.Box.Width, menuBoxHandler.Box.GameMenus.Count);
+                menuBoxHandler.GameMenuFirstDisplay();
+                InventoryMenuBoxHandler.GameMenuSecondDisplay();
+                InventoryMenuItemBoxHandler.GameMenuFourthdDisplay(inputHandler, characterUI);
+
+                var key = inputHandler.GetUserInput();
+                InventoryMenuItemBoxHandler.Navigate(key);
+
+                if (key == ConsoleKey.Enter)
+                {
+                    InventoryMenuItemWorkUI.InventoryMenuItemWorkDisplay(inputHandler, characterUI);
+
+                }
+                else if (key == ConsoleKey.V)
+                {
+                    InventoryMenuDisplay(inputHandler, characterUI);
+                    conti = false;
+                }
+            }
+        }
+    }
+}
