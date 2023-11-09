2355b9e6e43f53eb6225ce6fec6c4fc97a688f06
loftdoggy <chtori1202@gmail.com>
2023-11-10 00:41:02 +0900

    PlayerWorkSelectionUI 클래스 생성 및 기능 구현 : 장비 및 소모품 선택 기능 구현

---
+++ Text_RPG/UI/PlayerMenuUI/CharacterMenuUI/PlayerWorkSelectionUI.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Text_RPG.UI.GameMenuBoxHandler;
+using Text_RPG.UI.PlayerMenuUI.InventoryUI;
+using Text_RPG.Util;
+
+namespace Text_RPG.UI.PlayerMenuUI.CharacterMenuUI
+{
+    class PlayerWorkSelectionUI : PlayerCharacterMenuUI
+    {
+        public PlayerWorkSelectionUI(CharacterSelectionUI characterSelectionUI) : base(characterSelectionUI)
+        {
+
+        }
+
+        private static readonly List<string> PlayerWorkSelectionNumber = new List<string>
+        {
+            "장비",
+            "소모품"
+        };
+
+        protected static readonly GameBoxHandler PlayerWorkSelectionBoxHandler = new GameBoxHandler(PlayerWorkSelectionNumber);
+
+        public void PlayerWorkSelectionDisplay(InputHandler inputHandler, CharacterSelectionUI characterUI)
+        {
+
+            bool conti = true;
+            while (conti)
+            {
+                Console.Clear();
+                UIRender.DrawCharacterMenuBox(menuBoxHandler.Box.Width, menuBoxHandler.Box.GameMenus.Count);
+                UIRender.DrawCharacterMenuWorkBox(menuBoxHandler.Box.Width, menuBoxHandler.Box.GameMenus.Count);
+                menuBoxHandler.GameMenuFirstDisplay();
+                PlayerMenuBoxHandler.GameMenuSecondOnlyPlayerDisplay();
+                PlayerMenuNameBoxHandler.GameMenuFourthOnlyPlayerDisplay();
+                PlayerWorkSelectionBoxHandler.GameMenuThirdDisplay();
+
+                var key = inputHandler.GetUserInput();
+                PlayerWorkSelectionBoxHandler.Navigate(key);
+
+                if (key == ConsoleKey.Enter)
+                {
+                    PlayerWorkSelectionPerformAction(inputHandler, characterUI, PlayerWorkSelectionBoxHandler.Box.SelectedIndex);
+                    conti = false;
+                }
+                else if (key == ConsoleKey.V)
+                {
+                    PlayerMenuDisplay(inputHandler, characterUI);
+                    conti = false;
+                }
+                else if(key == ConsoleKey.C)
+                {
+                    DisplayCaharcterMenu(inputHandler, characterUI);
+                    conti = false;
+                }
+            }
+        }
+
+        protected static void PlayerWorkSelectionPerformAction(InputHandler inputHandler, CharacterSelectionUI characterUI, int selectedIndex)
+        {
+            switch (selectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+            }
+        }
+    }
+}
