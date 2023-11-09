5eb3ddc3a9a9f516c9dbe9c088c899ccea754ae0
loftdoggy <chtori1202@gmail.com>
2023-11-10 00:39:07 +0900

    기능 추가 : 메뉴 선택으로 가기 기능 구현

---
+++ Text_RPG/UI/PlayerMenuUI/InventoryUI/InventoryMenuItemWorkUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Text_RPG.UI.GameMenuBoxHandler;
+using Text_RPG.Util;
+
+namespace Text_RPG.UI.PlayerMenuUI.InventoryUI
+{
+    class InventoryMenuItemWorkUI : InventoryMenuItemUI
+    {
+        private static readonly List<string> InventoryMenuItemWorkNumber = new List<string> { "버리기", "정렬(구현 미정)" };
+
+        protected static readonly GameBoxHandler InventoryMenuItemWorkBoxHandler = new GameBoxHandler(InventoryMenuItemWorkNumber);
+
+        public static void InventoryMenuItemWorkDisplay(InputHandler inputHandler, CharacterSelectionUI characterUI)
+        {
+            bool conti = true;
+            while (conti)
+            {
+                Console.Clear();
+                UIRender.DrawCharacterMenuBox(menuBoxHandler.Box.Width, menuBoxHandler.Box.GameMenus.Count);
+                UIRender.DrawInventoryMenuWorkBox(menuBoxHandler.Box.Width, menuBoxHandler.Box.GameMenus.Count);
+                menuBoxHandler.GameMenuFirstDisplay();
+                InventoryMenuBoxHandler.GameMenuSecondDisplay();
+                InventoryMenuItemBoxHandler.GameMenuFourthDisplay();
+                InventoryMenuItemWorkBoxHandler.GameMenuThirdDisplay();
+
+                var key = inputHandler.GetUserInput();
+                InventoryMenuItemWorkBoxHandler.Navigate(key);
+
+                if (key == ConsoleKey.Enter)
+                {
+                    InventoryMenuItemWorkPerformAction(inputHandler, characterUI, InventoryMenuItemWorkBoxHandler.Box.SelectedIndex);
+
+                }
+                else if (key == ConsoleKey.V)
+                {
+                    InventoryMenuItemDisplay(inputHandler, characterUI);
+                    conti = false;
+                }
+                else if (key == ConsoleKey.C)
+                {
+                    DisplayCaharcterMenu(inputHandler, characterUI);
+                    conti = false;
+                }
+            }
+        }
+
+        public static void InventoryMenuItemWorkPerformAction(InputHandler inputHandler, CharacterSelectionUI characterUI, int selectedIndex)
+        {
+            switch (selectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+            }
+        }
+    }
+}
